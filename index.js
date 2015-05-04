15f7c845c9072cd0cc1b47f18553b6ef8f026cfb
pemrouz <p3pedram@hotmail.com>
2015-05-04 23:33:49 +0100

    reuse to module

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var is = require('is')
+  , to = require('to')
 
 module.exports = function log(prefix){
   return function(d){
     is.arr(arguments[2]) && (arguments[2] = arguments[2].length)
-    var args = [].slice.call(arguments, 0)
+    var args = to.arr(arguments)
     args.unshift(''.grey ? prefix.grey : prefix)
     return console.log.apply(console, args), d
   }
