395a523006a3e62347507bebacb334b1e8b10bab
pemrouz <p3pedram@hotmail.com>
2015-05-04 17:40:40 +0100

    more graceful output when used functionally

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var is = require('is')
+
 module.exports = function log(prefix){
   return function(d){
+    is.arr(arguments[2]) && (arguments[2] = arguments.length)
     return console.log.bind(console, ''.grey ? prefix.grey : prefix).apply(console, arguments), d
   }
 }
