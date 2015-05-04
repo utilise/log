26f334fc78cbaef136888258265331617b2835e3
pemrouz <p3pedram@hotmail.com>
2015-05-04 17:17:59 +0100

    update color check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function log(prefix){
   return function(d){
-    return console.log.bind(console, String.grey ? prefix.grey : prefix).apply(console, arguments), d
+    return console.log.bind(console, ''.grey ? prefix.grey : prefix).apply(console, arguments), d
   }
 }
