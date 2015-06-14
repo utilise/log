1fe70afb0374fffdbb9351a1d5f4af0346d90c6f
pemrouz <p3pedram@hotmail.com>
2015-06-14 20:45:33 +0100

    100% coverage

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var is = require('is')
   , to = require('to')
+  , owner = require('owner')
 
 module.exports = function log(prefix){
   return function(d){
+    if (!owner.console) return d;
     is.arr(arguments[2]) && (arguments[2] = arguments[2].length)
     var args = to.arr(arguments)
-    args.unshift(''.grey ? prefix.grey : prefix)
+    args.unshift(prefix.grey ? prefix.grey : prefix)
     return console.log.apply(console, args), d
   }
 }
--- test.js
+++ test.js
@@ -1,9 +1,51 @@
 var expect = require('chai').expect
-  , log = require('./')('sth')
+  , owner = require('owner')
+  , log = require('./')
+  , to = require('to')
 
 describe('log', function() {
   it('should log value untouched and return it', function() {
     var o = { a: 1 }
-    expect(log(o)).to.equal(o)
+    expect(log('foo')(o)).to.equal(o)
   })
+
+  it('should do nothing if no console', function() {
+    var realLog = owner.console.log
+      , realConsole = owner.console
+
+    delete owner.console
+    expect(log('foo')('bar')).to.equal('bar')
+    owner.console = realConsole
+  })
+
+  it('should loop over arrays compactly', function() {
+    var realLog = owner.console.log
+      , realConsole = owner.console
+      , result
+
+    delete owner.console
+    owner.console = { log: function(){ result = to.arr(arguments).pop(); realLog.apply(this, arguments) } }
+    ;[1,2,3].map(log('foo'))
+    expect(result).to.equal(3)
+    owner.console = realConsole
+  })
+
+  it('should print in color if exists', function() {
+    var prefix = 'foo'
+      , realGrey = String.prototype.grey
+      , realLog = owner.console.log
+      , realConsole = owner.console
+      , result
+
+    delete owner.console
+    owner.console = { log: function(){ result = to.arr(arguments).shift(); realLog.apply(this, arguments) } }
+    String.prototype.grey = 'baz'
+
+    expect(log(prefix)('bar')).to.equal('bar')
+    expect(result).to.equal('baz')
+
+    owner.console = realConsole
+    String.prototype.grey = realGrey
+  })
+
 })
