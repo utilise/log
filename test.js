980770f286ed02f8495bd2b133fe1c7c92fc8629
pemrouz <p3pedram@hotmail.com>
2016-08-14 22:16:23 +0100

    feat: add timestamps

--- test.js
+++ test.js
@@ -1,56 +1,70 @@
 var expect = require('chai').expect
   , client = require('utilise.client')
-  , owner = require('utilise.owner')
-  , log = require('./')
-  , to = require('utilise.to')
+  , owner  = require('utilise.owner')
+  , to     = require('utilise.to')
+  , log    = require('./')
+  , iso    = '\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d+([+-][0-2]\\d:[0-5]\\d|Z)'
+  , output, realLog, realConsole
 
 describe('log', function() {
-  it('should log value untouched and return it', function() {
-    var o = { a: 1 }
-    expect(log('foo')(o)).to.equal(o)
+
+  it('should do nothing if no console.log', function() {
+    init(function(){
+      delete owner.console
+      expect(log('[foo]')('bar')).to.equal('bar')
+      expect(output).to.not.be.ok
+    })
   })
 
-  it('should do nothing if no console', function() {
-    var realConsole = owner.console
-    delete owner.console
-    expect(log('foo')('bar')).to.equal('bar')
-    owner.console = realConsole
+  it('should log value untouched and return it', function() {
+    init(function(){
+      var o = { a: 1 }
+      expect(log('[foo]')(o)).to.equal(o)
+      expect(reg('foo').test(output)).to.be.ok
+    })
   })
 
   it('should loop over arrays compactly', function() {
-    if (!owner.console) return
-    var realLog = owner.console.log
-      , realConsole = owner.console
-      , result
-
-    delete owner.console
-    owner.console = { log: function(){ 
-      result = to.arr(arguments).pop(); 
-      realLog.apply && realLog.apply(realConsole, arguments) } }
-    ;[1,2,3].map(log('foo'))
-    expect(result).to.equal(3)
-    owner.console = realConsole
+    init(function(){
+      ['a','b','c'].map(log('[foo]'))
+      expect(reg('foo').test(output)).to.be.ok
+      expect(reg('foo', ' a 0 3').test(output)).to.be.ok
+      expect(reg('foo', ' b 1 3').test(output)).to.be.ok
+      expect(reg('foo', ' c 2 3').test(output)).to.be.ok
+    })
   })
 
   !client && it('should print in color if exists', function() {
-    if (!owner.console) return
-    var prefix = 'foo'
-      , realGrey = String.prototype.grey
-      , realLog = owner.console.log
-      , realConsole = owner.console
-      , result
+    init(function(){
+      var realGrey = String.prototype.grey
 
-    delete owner.console
-    owner.console = { log: function(){ 
-      result = to.arr(arguments).shift(); 
-      realLog.apply && realLog.apply(realConsole, arguments) } }
-    String.prototype.grey = 'baz'
+      String.prototype.grey = 'grey_string'
 
-    expect(log(prefix)('bar')).to.equal('bar')
-    expect(result).to.equal('baz')
+      expect(log('foo')('bar')).to.equal('bar')
+      expect(output).to.be.eql('grey_string bar\n')
 
-    owner.console = realConsole
-    String.prototype.grey = realGrey
+      String.prototype.grey = realGrey
+    })
   })
 
 })
+
+function init(fn){
+  output = '' 
+  /* istanbul ignore next */
+  if (!owner.console) return
+  realConsole = owner.console
+  realLog     = owner.console.log
+  owner.console.log = function(){
+    realLog.apply(realConsole, arguments)
+    output += to.arr(arguments).join(' ') + '\n'
+  }
+  try { fn() } finally {
+    owner.console     = realConsole
+    owner.console.log = realLog
+  }
+}
+
+function reg(ns, info) {
+  return new RegExp('\\[log\\]\\['+iso+'\\]\\['+ns+'\\]' + (info || ''), 'mg')
+}
